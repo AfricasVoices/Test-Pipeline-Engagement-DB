f39b9b3f85477275227bbf9b92546ef451d37ae9
Alexander Simpson <as2388@cam.ac.uk>
2021-06-08 16:29:44 +0100

    Support syncing consent withdrawn status back to Rapid Pro

--- src/engagement_db_to_rapid_pro/configuration.py
+++ src/engagement_db_to_rapid_pro/configuration.py
@@ -24,7 +24,7 @@ class DatasetConfiguration:
 @dataclass
 class EngagementDBToRapidProConfiguration:
     normal_datasets: Optional[List[DatasetConfiguration]] = None
-    # TODO: consent_withdrawn configuration
+    consent_withdrawn_dataset: Optional[DatasetConfiguration] = None
     write_mode: str = WriteModes.SHOW_PRESENCE
     allow_clearing_fields: bool = False  # Whether to allow setting contact fields to empty. Setting this to True may
                                          # not be appropriate for continuous sync because a new message may have arrived
