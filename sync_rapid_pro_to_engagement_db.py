e33c1dea1358a6ed766b289a0b3dcb829f138cf0
Alexander Simpson <as2388@cam.ac.uk>
2021-07-08 15:31:03 +0100

    Support importing from Rapid Pro -> engagement db from archives.

--- sync_rapid_pro_to_engagement_db.py
+++ sync_rapid_pro_to_engagement_db.py
@@ -5,6 +5,7 @@ import subprocess
 from core_data_modules.logging import Logger
 from engagement_database.data_models import HistoryEntryOrigin
 
+from src.rapid_pro_to_engagement_db.rapid_pro_archive_client import RapidProArchiveClient
 from src.rapid_pro_to_engagement_db.rapid_pro_to_engagement_db import sync_rapid_pro_to_engagement_db
 
 log = Logger(__name__)
@@ -14,6 +15,11 @@ if __name__ == "__main__":
 
     parser.add_argument("--incremental-cache-path",
                         help="Path to a directory to use to cache results needed for incremental operation.")
+    parser.add_argument("--local-archive", action="append",
+                        help="Configures a local archive directory to use in place of a production Rapid Pro "
+                             "workspace, in the form '<gs-url>=<local-path>' "
+                             "e.g. --local-archive gs://bucket/test.json=~/test-archive. "
+                             "To configure multiple local archives, pass multiple --local-archive flags")
     parser.add_argument("user", help="Identifier of the user launching this program")
     parser.add_argument("google_cloud_credentials_file_path", metavar="google-cloud-credentials-file-path",
                         help="Path to a Google Cloud service account credentials file to use to access the "
@@ -25,9 +31,16 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     incremental_cache_path = args.incremental_cache_path
+    local_archives = args.local_archive
     user = args.user
     google_cloud_credentials_file_path = args.google_cloud_credentials_file_path
     pipeline_config = importlib.import_module(args.configuration_module).PIPELINE_CONFIGURATION
+
+    local_archives_map = dict()
+    for archive_arg in local_archives:
+        gs_url = archive_arg.split("=")[0]
+        local_path = archive_arg.split("=")[1]
+        local_archives_map[gs_url] = local_path
 
     pipeline = pipeline_config.pipeline_name
     commit = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
@@ -44,7 +57,16 @@ if __name__ == "__main__":
 
     for i, rapid_pro_config in enumerate(pipeline_config.rapid_pro_sources):
         log.info(f"Syncing Rapid Pro source {i + 1}/{len(pipeline_config.rapid_pro_sources)}...")
-        rapid_pro = rapid_pro_config.rapid_pro.init_rapid_pro_client(google_cloud_credentials_file_path)
+
+        # If a local archive was specified for this gs url, use a rapid pro archive client, otherwise connect to the
+        # production workspace.
+        rapid_pro_token_url = rapid_pro_config.rapid_pro.token_file_url
+        if rapid_pro_token_url in local_archives_map:
+            log.info(f"Overriding Rapid Pro source {rapid_pro_token_url} with local archive "
+                     f"{local_archives_map[rapid_pro_token_url]}")
+            rapid_pro = RapidProArchiveClient(local_archives_map[rapid_pro_token_url])
+        else:
+            rapid_pro = rapid_pro_config.rapid_pro.init_rapid_pro_client(google_cloud_credentials_file_path)
 
         sync_rapid_pro_to_engagement_db(
             rapid_pro, engagement_db, uuid_table, rapid_pro_config.flow_results, incremental_cache_path)
