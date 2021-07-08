a29b190e5d43f27eeb75e75486ff7f807cc6cb3e
Alexander Simpson <as2388@cam.ac.uk>
2021-07-08 17:16:48 +0100

    Fix type error of datetime filters

--- configurations/test_pipeline_configuration.py
+++ configurations/test_pipeline_configuration.py
@@ -8,9 +8,9 @@ from src.pipeline_configuration_spec import *
 PIPELINE_CONFIGURATION = PipelineConfiguration(
     pipeline_name="engagement-db-test",
     # TODO: store in messages and individuals_filter list of functions.
-    project_start_date = isoparse("2021-03-01T10:30:00+03:00"),
-    project_end_date = isoparse("2100-01-01T00:00:00+03:00"),
-    filter_test_messages = False,
+    project_start_date=isoparse("2021-03-01T10:30:00+03:00"),
+    project_end_date=isoparse("2100-01-01T00:00:00+03:00"),
+    filter_test_messages=False,
     engagement_database=EngagementDatabaseClientConfiguration(
         credentials_file_url="gs://avf-credentials/firebase-test.json",
         database_path="engagement_db_experiments/experimental_test"
