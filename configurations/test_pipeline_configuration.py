75595c4aa50ad64d7910b97b95852da0b606bf81
Isaack Mwenda <mwendaisaac@gmail.com>
2021-07-14 11:00:05 +0300

    fix spacing consistency.
    Co-authored-by: Alexander Simpson <as2388@cam.ac.uk>

--- configurations/test_pipeline_configuration.py
+++ configurations/test_pipeline_configuration.py
@@ -8,10 +8,10 @@ from src.pipeline_configuration_spec import *
 PIPELINE_CONFIGURATION = PipelineConfiguration(
     pipeline_name="engagement-db-test",
     # TODO: store in messages and individuals_filter list of functions.
-    project_start_date = isoparse("2021-03-01T10:30:00+03:00"),
-    project_end_date = isoparse("2100-01-01T00:00:00+03:00"),
-    filter_test_participants = False,
-    test_participant_uids = [
+    project_start_date=isoparse("2021-03-01T10:30:00+03:00"),
+    project_end_date=isoparse("2100-01-01T00:00:00+03:00"),
+    filter_test_participants=False,
+    test_participant_uids=[
         "avf-participant-uuid-51c15546-58a0-4ab1-b465-e65b71462a8f"
     ],
     engagement_database=EngagementDatabaseClientConfiguration(
