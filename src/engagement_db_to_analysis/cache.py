dbed2fd16426178828b73739d99bf52bd1683db7
Alexander Simpson <as2388@cam.ac.uk>
2021-07-20 15:24:46 +0100

    Use message timestamp serializer from Pipeline-Infrastructure

--- src/engagement_db_to_analysis/cache.py
+++ src/engagement_db_to_analysis/cache.py
@@ -4,6 +4,7 @@ import json
 
 
 from core_data_modules.util import IOUtils
+from engagement_database.data_models import Message
 
 
 class AnalysisCache(object):
@@ -16,9 +17,7 @@ class AnalysisCache(object):
         :param cache_dir: Directory to use for the cache.
         :type cache_dir: str
         """
-
         self.cache_dir = cache_dir
-
 
     def _latest_message_timestamp_path(self, engagement_db_dataset):
         return f"{self.cache_dir}/last_updated_{engagement_db_dataset}.txt"
@@ -32,7 +31,6 @@ class AnalysisCache(object):
         :return: Timestamp for the last updated message in cache, or None if there is no cache yet for this context.
         :rtype: datetime.datetime | None
         """
-
         try:
             with open(self._latest_message_timestamp_path(engagement_db_dataset)) as f:
                 return datetime.fromisoformat(f.read())
@@ -48,38 +46,41 @@ class AnalysisCache(object):
         :return: Latest run timestamp.
         :rtype: datetime.datetime
         """
-
         export_path = self._latest_message_timestamp_path(engagement_db_dataset)
         IOUtils.ensure_dirs_exist_for_file(export_path)
         with open(export_path, "w") as f:
             f.write(last_updated.isoformat())
 
-    def get_previous_export_messages(self, engagement_db_dataset):
+    def get_messages(self, engagement_db_dataset):
         """
-        Imports a list of messages for the given engagement_db_dataset from cache.
+        Gets a list of messages for the given engagement_db_dataset from the cache.
 
         :param engagement_db_dataset: Engagement db dataset name for this context.
         :type engagement_db_dataset: str
         :return: list of messages
-        :rtype: list
+        :rtype: list of engagement_database.data_models.Message
         """
+        previous_export_file_path = path.join(f"{self.cache_dir}/{engagement_db_dataset}.jsonl")
+        messages = []
+        try:
+            with open(previous_export_file_path) as f:
+                for line in f:
+                    messages.append(Message.from_dict(json.loads(line)))
+        except FileNotFoundError:
+            return []
 
-        previous_export_file_path = path.join(f"{self.cache_dir}/{engagement_db_dataset}.json")
-        with open(previous_export_file_path) as f:
-            previous_export = json.load(f)
+        return messages
 
-        return previous_export
-
-    def export_engagement_db_dataset(self, engagement_db_dataset, messages):
+    def set_messages(self, engagement_db_dataset, messages):
         """
-        Exports a list of messages for the given engagement_db_dataset to cache.
+        Sets a list of messages for the given engagement_db_dataset.
 
         :param engagement_db_dataset: Engagement db dataset name for this context.
         :type engagement_db_dataset: str
-        :return: list of messages
-        :rtype: list
+        :param messages: Messages to set.
+        :type messages: list of engagement_database.data_models.Message
         """
-
-        export_file_path = path.join(f"{self.cache_dir}/{engagement_db_dataset}.json")
-        with open(export_file_path, 'w') as f:
-            json.dump(messages, f, indent=2)
+        export_file_path = path.join(f"{self.cache_dir}/{engagement_db_dataset}.jsonl")
+        with open(export_file_path, "w") as f:
+            for msg in messages:
+                f.write(f"{json.dumps(msg.to_dict(serialize_datetimes_to_str=True))}\n")
