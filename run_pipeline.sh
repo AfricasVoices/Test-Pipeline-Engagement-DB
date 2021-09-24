0c4edb49d060593b5cea2bf56a48117310bf7286
IsaackMwenda <mwendaisaac@gmail.com>
2021-09-24 16:50:03 +0300

    add run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,52 +2,37 @@
 
 set -e
 
-if [[ $# -ne 10 ]]; then
+if [[ $# -ne 4 ]]; then
     echo "Usage: ./run_pipeline.sh"
-    echo "  <user> <pipeline-run-mode> <pipeline-configuration-json>"
-    echo "  <coda-pull-credentials-path> <coda-push-credentials-path> <avf-bucket-credentials-path>"
-    echo "  <coda-tools-root> <data-root> <data-backup-dir> <performance-logs-dir>"
-    echo "Runs the pipeline end-to-end (data fetch, coda fetch, output generation, Drive upload, Coda upload, data backup)"
+    echo "  <user> <google-cloud-credentials-file-path> <configuration-module> <output-dir>"
+    echo "Runs the pipeline end-to-end (sync-rapid-pro-to-engagement-db, sync-engagement-db-to-coda, sync-coda-to-engagement-db,\
+          sync-engagement-db-to-rapid-pro, run-engagement-db-to-analysis,)"
     exit
 fi
 
 USER=$1
-PIPELINE_RUN_MODE=$2
-PIPELINE_CONFIGURATION=$3
-CODA_PULL_CREDENTIALS_PATH=$4
-CODA_PUSH_CREDENTIALS_PATH=$5
-AVF_BUCKET_CREDENTIALS_PATH=$6
-CODA_TOOLS_ROOT=$7
-DATA_ROOT=$8
-DATA_BACKUPS_DIR=$9
-PERFORMANCE_LOGS_DIR=${10}
+GOOGLE_CLOUD_CREDENTIALS_PATH=$2
+CONFIGURATION_MODULE=$3
+OUTPUT_DIR=$4
 
 DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 HASH=$(git rev-parse HEAD)
 RUN_ID="$DATE-$HASH"
 
-echo "Starting run with id '$RUN_ID'"
+./docker-run-log-pipeline-event.sh  "$CONFIGURATION_MODULE" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$RUN_ID" "PipelineRunStart"
 
-./log_pipeline_event.sh "$USER" "$AVF_BUCKET_CREDENTIALS_PATH" "$PIPELINE_CONFIGURATION" "$RUN_ID" "PipelineRunStart"
+./docker-sync-rapid-pro-to-engagement-db.sh --incremental-cache-volume "cache" "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" \
+                                    "$CONFIGURATION_MODULE"
 
-./1_coda_get.sh "$CODA_PULL_CREDENTIALS_PATH" "$CODA_TOOLS_ROOT" "$DATA_ROOT"
+./docker-sync-engagement-db-to-coda.sh --incremental-cache-volume "cache" "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" \
+                        "$CONFIGURATION_MODULE"
 
-./2_fetch_raw_data.sh "$USER" "$AVF_BUCKET_CREDENTIALS_PATH" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
+./docker-sync-coda-to-engagement-db.sh --incremental-cache-volume "cache" "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" \
+                        "$CONFIGURATION_MODULE"
 
-./3_generate_outputs.sh --profile-memory "$PERFORMANCE_LOGS_DIR/memory-$RUN_ID.profile" \
-    "$USER" "$PIPELINE_RUN_MODE" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
+./docker-sync-engagement-db-to-rapid-pro.sh "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$CONFIGURATION_MODULE"
 
-./4_coda_add.sh "$CODA_PUSH_CREDENTIALS_PATH" "$CODA_TOOLS_ROOT" "$DATA_ROOT"
+./docker-run-engagement-db-to-analysis.sh --incremental-cache-volume "cache" "$USER" "$GOOGLE_CLOUD_CREDENTIALS_PATH" \
+                        "$CONFIGURATION_MODULE" "$OUTPUT_DIR"
 
-if [[ $PIPELINE_RUN_MODE == "all-stages" ]]; then
-   ./5_automated_analysis.sh --profile-memory "$PERFORMANCE_LOGS_DIR/automated-analysis-memory-$RUN_ID.profile" "$USER" "$PIPELINE_CONFIGURATION" "$DATA_ROOT"
-fi
-
-./6_backup_data_root.sh "$DATA_ROOT" "$DATA_BACKUPS_DIR/data-$RUN_ID.tar.gzip"
-
-./7_upload_analysis_files.sh "$USER" "$PIPELINE_RUN_MODE" "$AVF_BUCKET_CREDENTIALS_PATH" "$PIPELINE_CONFIGURATION" \
-    "$RUN_ID" "$DATA_ROOT"
-
-./8_upload_log_files.sh "$USER" "$AVF_BUCKET_CREDENTIALS_PATH" "$PIPELINE_CONFIGURATION" "$PERFORMANCE_LOGS_DIR" "$DATA_BACKUPS_DIR"
-
-./log_pipeline_event.sh "$USER" "$AVF_BUCKET_CREDENTIALS_PATH" "$PIPELINE_CONFIGURATION" "$RUN_ID" "PipelineRunEnd"
+./docker-run-log-pipeline-event.sh  "$CONFIGURATION_MODULE" "$GOOGLE_CLOUD_CREDENTIALS_PATH" "$RUN_ID" "PipelineRunEnd"
