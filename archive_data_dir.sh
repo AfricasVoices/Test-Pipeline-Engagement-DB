398a366a3188c3510ddd36ef4447f0287cf8f5ca
IsaackMwenda <mwendaisaac@gmail.com>
2021-10-13 11:58:21 +0300

    nit - rename arguments

--- archive_data_dir.sh
+++ archive_data_dir.sh
@@ -3,16 +3,16 @@
 set -e
 
 if [[ $# -ne 2 ]]; then
-    echo "Usage: ./6_backup_data_root <data-root> <backup-location>"
+    echo "Usage: ./archive_data_dir <data-dir> <backup-file>"
     echo "Backs-up the data root directory to a compressed file in at the specified location"
     exit
 fi
 
-OUTPUT_DIR=$1
+DATA_DIR=$1
 BACKUP_FILE=$2
 
 mkdir -p "$(dirname "$BACKUP_FILE")"
-find "$OUTPUT_DIR" -type f -name '.DS_Store' -delete
-cd "$OUTPUT_DIR"
-echo "tarring into -czvf $OUTPUT_DIR"
+find "$DATA_DIR" -type f -name '.DS_Store' -delete
+cd "$DATA_DIR"
+echo "tarring into -czvf $DATA_DIR"
 tar -czvf "$BACKUP_FILE" .
