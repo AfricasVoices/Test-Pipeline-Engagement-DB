24511494797b12242a9c91f31cf50c8b0f01dc01
IsaackMwenda <mwendaisaac@gmail.com>
2021-10-26 10:30:13 +0300

    fix run script arguments

--- engagement_db_to_analysis.py
+++ engagement_db_to_analysis.py
@@ -12,8 +12,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--incremental-cache-path",
                         help="Path to a directory to use to cache results needed for incremental operation.")
-    parser.add_argument("--membership-group-path",
-                        help="Path to a directory to use to read & write membership group csvs to.")
+
     parser.add_argument("user", help="Identifier of the user launching this program")
     parser.add_argument("google_cloud_credentials_file_path", metavar="google-cloud-credentials-file-path",
                         help="Path to a Google Cloud service account credentials file to use to access the "
@@ -21,6 +20,8 @@ if __name__ == "__main__":
     parser.add_argument("configuration_module",
                         help="Configuration module to import e.g. 'configurations.test_config'. "
                              "This module must contain a PIPELINE_CONFIGURATION property")
+    parser.add_argument("membership_group_path",
+                        help="Path to a directory to use to read & write membership group csvs to.")
     parser.add_argument("output_dir", metavar="output-dir",
                         help="Directory to output all analysis results to. This script will create and organise the "
                              "outputs into sensible sub-directories automatically")
@@ -28,10 +29,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     incremental_cache_path = args.incremental_cache_path
-    membership_group_path = args.membership_group_path
     user = args.user
     google_cloud_credentials_file_path = args.google_cloud_credentials_file_path
     pipeline_config = importlib.import_module(args.configuration_module).PIPELINE_CONFIGURATION
+    membership_group_path = args.membership_group_path
     output_dir = args.output_dir
 
     pipeline = pipeline_config.pipeline_name
@@ -39,5 +40,5 @@ if __name__ == "__main__":
     uuid_table = pipeline_config.uuid_table.init_uuid_table_client(google_cloud_credentials_file_path)
     engagement_db = pipeline_config.engagement_database.init_engagement_db_client(google_cloud_credentials_file_path)
 
-    generate_analysis_files(user, google_cloud_credentials_file_path, pipeline_config, engagement_db, output_dir,
-                            incremental_cache_path, membership_group_path)
+    generate_analysis_files(user, google_cloud_credentials_file_path, pipeline_config, engagement_db, membership_group_path,
+    output_dir, incremental_cache_path)
