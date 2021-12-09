cbc8bbf7e15f7c5e8bebd4867baae73149c748c6
Daniel Mwendwa <danielmwendwam@gmail.com>
2021-12-09 12:51:40 +0300

    rename variable

--- src/engagement_db_coda_sync/lib.py
+++ src/engagement_db_coda_sync/lib.py
@@ -35,20 +35,20 @@ def ensure_coda_datasets_up_to_date(coda, coda_config, google_cloud_credentials_
     all_datasets_have_user_file_url = all(
         dataset_config.dataset_users_file_url is not None for dataset_config in coda_config.dataset_configurations)
 
-    default_repo_user_ids = []
+    default_project_user_ids = []
     if not all_datasets_have_user_file_url:
         assert coda_config.project_users_file_url is not None, \
          f"Specify user ids for coda datasets in CodaDatasetConfiguration or user ids for this project in CodaSyncConfiguration"
-        default_repo_user_ids = get_coda_users_from_gcloud(coda_config.project_users_file_url, google_cloud_credentials_file_path)
+        default_project_user_ids = get_coda_users_from_gcloud(coda_config.project_users_file_url, google_cloud_credentials_file_path)
 
     ws_correct_dataset_code_scheme = coda_config.ws_correct_dataset_code_scheme
     for dataset_config in coda_config.dataset_configurations:
-        repo_user_ids = []
+        config_user_ids = []
         if dataset_config.dataset_users_file_url:
-            repo_user_ids = get_coda_users_from_gcloud(dataset_config.dataset_users_file_url, google_cloud_credentials_file_path)
+            config_user_ids = get_coda_users_from_gcloud(dataset_config.dataset_users_file_url, google_cloud_credentials_file_path)
         else:
-            repo_user_ids = default_repo_user_ids
-        coda.set_dataset_user_ids(dataset_config.coda_dataset_id, repo_user_ids)
+            config_user_ids = default_project_user_ids
+        coda.set_dataset_user_ids(dataset_config.coda_dataset_id, config_user_ids)
 
         repo_code_schemes = []
         for code_scheme_config in dataset_config.code_scheme_configurations:
