22aca3fd6917965408396e3e81f37b7f3cdfe8ad
IsaackMwenda <mwendaisaac@gmail.com>
2022-03-27 11:52:53 +0300

    add telegram discussions configurations

---
+++ configurations/test_telegram_group_pipeline_configuration.py
@@ -0,0 +1,101 @@
+from core_data_modules.cleaners import swahili
+from dateutil.parser import isoparse
+
+from src.pipeline_configuration_spec import *
+
+PIPELINE_CONFIGURATION = PipelineConfiguration(
+    pipeline_name="engagement-db-telegram-group-test",
+    engagement_database=EngagementDatabaseClientConfiguration(
+        credentials_file_url="gs://avf-credentials/firebase-test.json",
+        database_path="engagement_db_experiments/experimental_test"
+    ),
+    uuid_table=UUIDTableClientConfiguration(
+        credentials_file_url="gs://avf-credentials/firebase-test.json",
+        table_name="_engagement_db_test",
+        uuid_prefix="avf-participant-uuid-"
+    ),
+    operations_dashboard=OperationsDashboardConfiguration(
+        credentials_file_url="gs://avf-credentials/avf-dashboards-firebase-adminsdk-gvecb-ef772e79b6.json",
+    ),
+    telegram_group_sources=[
+        TelegramGroupSource(
+            token_file_url="gs://avf-credentials/dev-telegram-tokens.txt",
+            datasets=[
+                TelegramGroupDataset(
+                    engagement_db_dataset="test_telegram_group_s01e01",
+                    search=TelegramGroupSearch(
+                        offset_date="2021-09-22T00:00+03:00",
+                    )
+                ),
+                TelegramGroupDataset(
+                    engagement_db_dataset="test_telegram_group_s01e02",
+                    search=TelegramGroupSearch(
+                        offset_date="2021-09-22T00:00+03:00",
+                    )
+                ),
+            ],
+        )
+    ],
+    coda_sync=CodaConfiguration(
+        coda=CodaClientConfiguration(credentials_file_url="gs://avf-credentials/coda-staging.json"),
+        sync_config=CodaSyncConfiguration(
+            dataset_configurations=[
+                CodaDatasetConfiguration(
+                    coda_dataset_id="TEST_Telegram_group_s01e01",
+                    engagement_db_dataset="test_telegram_s01e01",
+                    code_scheme_configurations=[
+                        CodeSchemeConfiguration(code_scheme=load_code_scheme("test_telegram_s01e01"), auto_coder=None,
+                                                coda_code_schemes_count=3)
+                    ],
+                    ws_code_string_value="test_telegram_s01e01"
+                ),
+                CodaDatasetConfiguration(
+                    coda_dataset_id="TEST_Telegram_group_s01e02",
+                    engagement_db_dataset="test_telegram_s01e02",
+                    code_scheme_configurations=[
+                        CodeSchemeConfiguration(code_scheme=load_code_scheme("test_telegram_s01e02"), auto_coder=None,
+                                                coda_code_schemes_count=3)
+                    ],
+                    ws_code_string_value="test_telegram_s01e02"
+                ),
+            ],
+            ws_correct_dataset_code_scheme=load_code_scheme("ws_correct_dataset"),
+            project_users_file_url="gs://avf-project-datasets/2021/TEST-PIPELINE-ENGAGEMENT-DB/coda_users.json"
+        )
+    ),
+    analysis=AnalysisConfiguration(
+        google_drive_upload=GoogleDriveUploadConfiguration(
+            credentials_file_url="gs://avf-credentials/pipeline-runner-service-acct-avf-data-core-64cc71459fe7.json",
+            drive_dir="pipeline_upload_test/telegram"
+        ),
+        dataset_configurations=[
+            AnalysisDatasetConfiguration(
+                engagement_db_datasets=["test_facebook_s01e01"],
+                dataset_type=DatasetTypes.RESEARCH_QUESTION_ANSWER,
+                raw_dataset="test_telegram_group_s01e01_raw",
+                coding_configs=[
+                    CodingConfiguration(
+                        code_scheme=load_code_scheme("test_telegram_group_s01e01"),
+                        analysis_dataset="test_telegram_group_s01e01"
+                    )
+                ]
+            ),
+            AnalysisDatasetConfiguration(
+                engagement_db_datasets=["test_telegram_group_s01e02"],
+                dataset_type=DatasetTypes.RESEARCH_QUESTION_ANSWER,
+                raw_dataset="test_telegram_group_s01e02_raw",
+                coding_configs=[
+                    CodingConfiguration(
+                        code_scheme=load_code_scheme("test_telegram_group_s01e02"),
+                        analysis_dataset="test_telegram_group_s01e02"
+                    )
+                ]
+            ),
+        ],
+        ws_correct_dataset_code_scheme=load_code_scheme("ws_correct_dataset"),
+    ),
+    archive_configuration=ArchiveConfiguration(
+        archive_upload_bucket="gs://pipeline-execution-backup-archive",
+        bucket_dir_path="2021/TEST-PIPELINE_DB"
+    )
+)
--- src/telegram_to_engagement_db/configuration.py
+++ src/telegram_to_engagement_db/configuration.py
@@ -1,49 +1,38 @@
 from dateutil.parser import isoparse
 
 
-class FacebookSource:
-    def __init__(self, page_id, token_file_url, datasets):
-        self.page_id = page_id
+class TelegramGroupSource:
+    def __init__(self, token_file_url, datasets):
         self.token_file_url = token_file_url
         self.datasets = datasets
 
     def to_dict(self):
         return {
-            "page_id": self.page_id,
             "token_file_url": self.token_file_url,
             "datasets": [dataset for dataset in self.datasets]
         }
 
 
-class FacebookDataset:
-    def __init__(self, engagement_db_dataset, post_ids=None, search=None):
+class TelegramGroupDataset:
+    def __init__(self, engagement_db_dataset, search):
         self.engagement_db_dataset = engagement_db_dataset
-        self.post_ids = post_ids
         self.search = search
 
-        if search is not None:
-            self.search = FacebookSearch.to_dict(search)
-
-        assert self.post_ids is not None or self.search is not None, \
-            "Must provide at least a post_id or search"
 
     def to_dict(self):
         return {
             "engagement_db_dataset":self.engagement_db_dataset,
-            "post_ids":self.post_ids,
             "search":self.search
         }
 
 
-class FacebookSearch:
-    def __init__(self, match, start_date, end_date):
-        self.match = match
-        self.start_date = start_date
-        self.end_date = end_date
+class TelegramGroupSearch:
+    def __init__(self, offset_date, max_id=None):
+        self.offset_date = offset_date
+        self.max_id = max_id
 
     def to_dict(self):
         return {
-            "match": self.match,
-            "start_date": isoparse(self.start_date),
-            "end_date": isoparse(self.end_date)
+            "offset_date": isoparse(self.offset_date),
+            "max_id": isoparse(self.max_id)
         }
