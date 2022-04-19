a43f15de579e6093e27e92505d7e82b2ad16bc80
IsaackMwenda <mwendaisaac@gmail.com>
2022-04-19 16:04:07 +0300

    create an interactive container

--- docker-run-telegram-group-to-engagement-db.sh
+++ docker-run-telegram-group-to-engagement-db.sh
@@ -21,8 +21,8 @@ done
 
 # Check that the correct number of arguments were provided.
 if [[ $# -ne 5 ]]; then
-    echo "Usage: $0 
-    [--incremental-cache-volume <incremental-cache-volume>] 
+    echo "Usage: $0
+    [--incremental-cache-volume <incremental-cache-volume>]
     <user> <google-cloud-credentials-file-path> <configuration-file> <code-schemes-dir> <data-dir>"
     exit
 fi
@@ -39,12 +39,12 @@ docker build -t "$IMAGE_NAME" .
 
 # Create a container from the image that was just built.
 CMD="pipenv run python -u sync_telegram_group_to_engagement_db.py ${INCREMENTAL_ARG} ${USER} \
-    /credentials/google-cloud-credentials.json configuration_file"
+    /credentials/google-cloud-credentials.json configuration"
 
 if [[ "$INCREMENTAL_ARG" ]]; then
-    container="$(docker container create -w /app --mount source="$INCREMENTAL_CACHE_VOLUME_NAME",target=/cache "$IMAGE_NAME" /bin/bash -c "$CMD")"
+    container="$(docker container create -t -w /app --mount source="$INCREMENTAL_CACHE_VOLUME_NAME",target=/cache "$IMAGE_NAME" /bin/bash -c "$CMD")"
 else
-    container="$(docker container create -w /app "$IMAGE_NAME" /bin/bash -c "$CMD")"
+    container="$(docker container create -t -w /app "$IMAGE_NAME" /bin/bash -c "$CMD")"
 fi
 
 echo "Created container $container"
@@ -62,7 +62,7 @@ docker cp "$CONFIGURATION_FILE" "$container:/app/configuration.py"
 
 # Run the container
 echo "Starting container $container_short_id"
-docker start -a -i "$container"
+docker start -a "$container"
 
 # Copy cache data out of the container for backup
 if [[ "$INCREMENTAL_ARG" ]]; then
