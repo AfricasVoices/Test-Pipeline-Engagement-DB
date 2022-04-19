3b376b067b546d7b4629d51aaa67332d4179afb1
Daniel Mwendwa <danielmwendwam@gmail.com>
2022-04-19 23:38:42 +0300

    update type hinting

--- src/rapid_pro_to_engagement_db/configuration.py
+++ src/rapid_pro_to_engagement_db/configuration.py
@@ -5,12 +5,12 @@ class FlowResultConfiguration:
     """_summary_
     """
 
-    def __init__(self, flow_name: str, flow_result_field: str, engagement_db_dataset: str) -> None:
+    def __init__(self, flow_name: str, flow_result_field: str, engagement_db_dataset: str) :
         self.flow_name = flow_name
         self.flow_result_field = flow_result_field
         self.engagement_db_dataset = engagement_db_dataset
 
-    def to_dict(self) -> Dict:
+    def to_dict(self) -> Dict[str, str]:
         return {
             "flow_name": self.flow_name,
             "flow_result_field": self.flow_result_field,
@@ -18,7 +18,7 @@ class FlowResultConfiguration:
         }
 
     @classmethod
-    def from_dict(cls, d: Dict[str, str]):
+    def from_dict(cls, d: Dict[str, str]) -> FlowResultConfiguration:
         flow_name = d["flow_name"]
         flow_result_field = d["flow_result_field"]
         engagement_db_dataset = d["engagement_db_dataset"]
