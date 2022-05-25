5e5a3578af6899e7c998fb8992c8c9fc7340cf49
Daniel Mwendwa <danielmwendwam@gmail.com>
2022-05-25 11:59:19 +0300

    update csv to db sync stats

--- src/csv_to_engagement_db/sync_stats.py
+++ src/csv_to_engagement_db/sync_stats.py
@@ -7,15 +7,17 @@ log = Logger(__name__)
 
 class CSVSyncEvents:
     READ_ROW_FROM_CSV = "read_row_from_csv"
+    ROW_EMPTY = "row_empty"
     MESSAGE_ALREADY_IN_ENGAGEMENT_DB = "message_already_in_engagement_db"
     ADD_MESSAGE_TO_ENGAGEMENT_DB = "add_message_to_engagement_db"
     MESSAGE_SKIPPED_NO_MATCHING_TIMESTAMP = "message_skipped_no_matching_timestamp"
 
 
-class CSVToEngagementDBSyncStats(SyncStats):
+class CsvDatasetToEngagementDBSyncStats(SyncStats):
     def __init__(self):
         super().__init__({
             CSVSyncEvents.READ_ROW_FROM_CSV: 0,
+            CSVSyncEvents.ROW_EMPTY: 0,
             CSVSyncEvents.MESSAGE_ALREADY_IN_ENGAGEMENT_DB: 0,
             CSVSyncEvents.ADD_MESSAGE_TO_ENGAGEMENT_DB: 0,
             CSVSyncEvents.MESSAGE_SKIPPED_NO_MATCHING_TIMESTAMP: 0
@@ -23,6 +25,7 @@ class CSVToEngagementDBSyncStats(SyncStats):
 
     def print_summary(self):
         log.info(f"CSV rows read: {self.event_counts[CSVSyncEvents.READ_ROW_FROM_CSV]}")
+        log.info(f"Engagement db datasets in csv: {self.event_counts[ENGAGEMENT_DB_DATASETS_IN_CSV]}")
         log.info(f"Messages already in engagement db: {self.event_counts[CSVSyncEvents.MESSAGE_ALREADY_IN_ENGAGEMENT_DB]}")
         log.info(f"Messages added to engagement db: {self.event_counts[CSVSyncEvents.ADD_MESSAGE_TO_ENGAGEMENT_DB]}")
         log.info(f"Messages skipped because they didn't match a dataset time-range: {self.event_counts[CSVSyncEvents.MESSAGE_SKIPPED_NO_MATCHING_TIMESTAMP]}")
