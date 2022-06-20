ddb37a908329db5e62b93b007e1a50859a42cef3
Daniel Mwendwa <danielmwendwam@gmail.com>
2022-06-20 23:43:54 +0300

    optional coda updates

--- sync_coda_to_engagement_db.py
+++ sync_coda_to_engagement_db.py
@@ -17,6 +17,8 @@ if __name__ == "__main__":
                         help="Logs the updates that would be made without updating anything.")
     parser.add_argument("--incremental-cache-path",
                         help="Path to a directory to use to cache results needed for incremental operation.")
+    parser.add_argument("-s", "--skip-coda-users-and-code-schemes-update", action="store_true",
+                        help="Whether to skip coda users and code schemes updates")
     parser.add_argument("user", help="Identifier of the user launching this program")
     parser.add_argument("google_cloud_credentials_file_path", metavar="google-cloud-credentials-file-path",
                         help="Path to a Google Cloud service account credentials file to use to access the "
@@ -51,5 +53,6 @@ if __name__ == "__main__":
     engagement_db = pipeline_config.engagement_database.init_engagement_db_client(google_cloud_credentials_file_path)
     coda = pipeline_config.coda_sync.coda.init_coda_client(google_cloud_credentials_file_path)
 
-    ensure_coda_datasets_up_to_date(coda, pipeline_config.coda_sync.sync_config, google_cloud_credentials_file_path, dry_run)
+    if not args.skip_coda_users_and_code_schemes_update:
+        ensure_coda_datasets_up_to_date(coda, pipeline_config.coda_sync.sync_config, google_cloud_credentials_file_path, dry_run)
     sync_coda_to_engagement_db(coda, engagement_db, pipeline_config.coda_sync.sync_config, incremental_cache_path, dry_run)
