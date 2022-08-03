45c27226391e7613fa099c49e5d758042164c08c
Daniel Mwendwa <danielmwendwam@gmail.com>
2022-08-03 14:07:51 +0300

    assert code scheme is not None

--- src/engagement_db_to_rapid_pro/engagement_db_to_rapid_pro.py
+++ src/engagement_db_to_rapid_pro/engagement_db_to_rapid_pro.py
@@ -168,7 +168,8 @@ def _labels_contain_consent_withdrawn(labels, code_schemes):
     """
     for label in labels:
         code_scheme = _code_scheme_for_label(label, code_schemes)
-        if code_scheme and code_scheme.get_code_with_code_id(label.code_id).control_code == Codes.STOP:
+        assert code_scheme is not None
+        if code_scheme.get_code_with_code_id(label.code_id).control_code == Codes.STOP:
             return True
 
     return False
