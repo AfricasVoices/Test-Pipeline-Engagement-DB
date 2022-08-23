9d4a5787cd6ba04a8bbff274c0b2df11dfb5d436
Daniel Mwendwa <danielmwendwam@gmail.com>
2022-08-23 22:02:10 +0300

    checks for number of engagement db messages to be merged
    Co-authored-by: Alexander Simpson <as2388@cam.ac.uk>

--- src/google_form_to_engagement_db/google_form_to_engagement_db.py
+++ src/google_form_to_engagement_db/google_form_to_engagement_db.py
@@ -339,7 +339,7 @@ def _sync_google_form_to_engagement_db(google_form_client, engagement_db, form_c
                         continue
                     messages.append(question_id_to_engagement_db_message[question_id])
                     messages_origin_details.append(question_id_to_message_origin_details[question_id])
-                if not len(messages) > 0:
+                if len(messages) == 0:
                     continue
                 elif len(messages) == 1:
                     [message], [message_origin_details] = messages, messages_origin_details
