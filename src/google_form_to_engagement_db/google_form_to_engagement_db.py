c6d7f4b32626bbfde2b30340d2214c4f4391965c
Daniel Mwendwa <danielmwendwam@gmail.com>
2022-08-25 17:50:58 +0300

    continue statement

--- src/google_form_to_engagement_db/google_form_to_engagement_db.py
+++ src/google_form_to_engagement_db/google_form_to_engagement_db.py
@@ -348,6 +348,7 @@ def _sync_google_form_to_engagement_db(google_form_client, engagement_db, form_c
                     continue
                 elif len(messages) == 1:
                     [message], [message_origin_details] = messages, messages_origin_details
+                    continue
                 else:
                     # TODO: Implement the function below that merges engagement db messages including their origin details
                     # message, message_origin_details = _merge_engagement_db_messages(messages, messages_origin_details, question_config.answers_delimeter)
