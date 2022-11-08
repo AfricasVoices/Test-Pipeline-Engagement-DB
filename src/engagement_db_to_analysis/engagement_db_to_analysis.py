008a4ae3062519d63f143dc5e14b087264a5a89a
Isaack Mwenda <mwendaisaac@gmail.com>
2022-11-08 20:05:58 +0300

    extract location based uuids

--- src/engagement_db_to_analysis/engagement_db_to_analysis.py
+++ src/engagement_db_to_analysis/engagement_db_to_analysis.py
@@ -79,65 +79,6 @@ def generate_analysis_files(user, google_cloud_credentials_file_path, pipeline_c
         pipeline_config.analysis.ws_correct_dataset_code_scheme
     )
 
-    messages_by_column = convert_to_messages_column_format(user, messages_traced_data, pipeline_config.analysis)
-    
-    '''
-    participants_by_column = convert_to_participants_column_format(user, messages_traced_data, pipeline_config.analysis)
-
-    log.info(f"Imputing messages column-view traced data...")
-    impute_codes_by_column_traced_data(user, messages_by_column, pipeline_config.analysis.dataset_configurations)
-
-    log.info(f"Imputing participants column-view traced data...")
-    impute_codes_by_column_traced_data(user, participants_by_column, pipeline_config.analysis.dataset_configurations)
-
-    # Export to hard-coded files for now.
-    export_production_file(messages_by_column, pipeline_config.analysis, f"{output_dir}/production.csv")
-
-    if pipeline_config.analysis.membership_group_configuration is not None:
-
-        membership_group_csv_urls = pipeline_config.analysis.membership_group_configuration.membership_group_csv_urls.items()
-        log.info("Tagging membership group participants to messages_by_column traced data...")
-        tag_membership_groups_participants(user, google_cloud_credentials_file_path, messages_by_column,
-                                           membership_group_csv_urls, membership_group_dir_path)
-
-        log.info("Tagging membership group participants to participants_by_column traced data...")
-        tag_membership_groups_participants(user, google_cloud_credentials_file_path, participants_by_column,
-                                           membership_group_csv_urls, membership_group_dir_path)
-
-    export_analysis_file(messages_by_column, pipeline_config, f"{output_dir}/messages.csv", export_timestamps=True)
-    export_analysis_file(participants_by_column, pipeline_config, f"{output_dir}/participants.csv")
-    '''
-    export_traced_data(messages_by_column, f"{output_dir}/messages.jsonl")
-
-    '''
-    export_traced_data(participants_by_column, f"{output_dir}/participants.jsonl")
-    
-    run_automated_analysis(messages_by_column, participants_by_column, pipeline_config.analysis, f"{output_dir}/automated-analysis")
-
-    dry_run_text = "(dry run)" if dry_run else ""
-    if pipeline_config.analysis.google_drive_upload is None:
-        log.debug(f"Not uploading to Google Drive, because the 'google_drive_upload' configuration was None {dry_run_text}")
-    else:
-        if dry_run:
-            log.info(f"Not uploading to Google Drive {dry_run_text}")
-        else:
-            log.info("Uploading outputs to Google Drive...")
-            google_drive_upload.init_client(
-                google_cloud_credentials_file_path,
-                pipeline_config.analysis.google_drive_upload.credentials_file_url
-            )
-
-            drive_dir = pipeline_config.analysis.google_drive_upload.drive_dir
-            google_drive_upload.upload_file(f"{output_dir}/production.csv", drive_dir)
-            google_drive_upload.upload_file(f"{output_dir}/messages.csv", drive_dir)
-            google_drive_upload.upload_file(f"{output_dir}/participants.csv", drive_dir)
-            google_drive_upload.upload_all_files_in_dir(
-                f"{output_dir}/automated-analysis", f"{drive_dir}/automated-analysis", recursive=True
-            )
-
-    if pipeline_config.rapid_pro_target is not None and pipeline_config.rapid_pro_target.sync_config.sync_advert_contacts:
-        sync_advert_contacts_to_rapid_pro(
-            participants_by_column, uuid_table, pipeline_config, rapid_pro,
-            google_cloud_credentials_file_path, membership_group_dir_path, cache_path, dry_run
-        )
-    '''
+    for message in messages_traced_data:
+        print(message)
+        exit()
