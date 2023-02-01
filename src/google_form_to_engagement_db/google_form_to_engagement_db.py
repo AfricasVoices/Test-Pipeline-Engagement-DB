8cfca51a1d1bbcfebe1c6ee61003e11a1b411aa4
Daniel Mwendwa <danielmwendwam@gmail.com>
2023-02-01 13:05:59 +0300

    check participant_uuid is not None
    Co-authored-by: Alexander Simpson <as2388@cam.ac.uk>

--- src/google_form_to_engagement_db/google_form_to_engagement_db.py
+++ src/google_form_to_engagement_db/google_form_to_engagement_db.py
@@ -200,7 +200,7 @@ def _merge_engagement_db_messages(messages_with_origin_details, answers_delimete
             participant_uuid, dataset = msg.participant_uuid, msg.dataset
             continue
         
-        assert msg.participant_uuid == participant_uuid and not None
+        assert msg.participant_uuid == participant_uuid and participant_uuid not None
         assert msg.dataset == dataset and not None
 
     text, timestamp = answers_delimeter.join(texts), sorted(timestamps)[-1]
